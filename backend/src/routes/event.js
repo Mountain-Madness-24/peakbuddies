2216af51155451006a87df0d437fbb08331bbb4f
zach1502 <zach1502@hotmail.com>
2024-03-23 16:50:00 -0700

    re-add auth route

--- backend/src/routes/event.js
+++ backend/src/routes/event.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../models/');
+const User = require('../models/user');
 
 
 router.post('/createEvent', async (req, res) => {
