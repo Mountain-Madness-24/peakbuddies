c4265e3b0855a072aaa3a9533601c0ccc7b80983
Kiaan Castillo <kiaan_castillo@sfu.ca>
2024-03-23 19:40:00 -0700

    Meeting page WIP

--- frontend/src/components/icons/index.js
+++ frontend/src/components/icons/index.js
@@ -4,3 +4,5 @@ export { IconSchool } from "./icon-school";
 export { IconWork } from "./icon-work";
 export { IconLocation } from "./icon-location";
 export { IconTime } from "./icon-time";
+export { IconIntro } from "./icon-intro";
+export { IconIcebreaker } from "./icon-icebreaker";
