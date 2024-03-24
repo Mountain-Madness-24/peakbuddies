2c1491667fdf42f51221d5ba2ec707a8c31b3d4c
Kiaan Castillo <kiaan_castillo@sfu.ca>
2024-03-23 23:38:54 -0700

    Finish event page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { LoginPage, NewMeetingPage, HomePage, MeetingPage } from "./pages";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import {
+  LoginPage,
+  NewMeetingPage,
+  HomePage,
+  MeetingPage,
+  EventPage,
+} from "./pages";
 
 const App = () => {
   return (
@@ -9,6 +20,7 @@ const App = () => {
         <Route path="/home/:id" element={<HomePage />} />
         <Route path="/new-meeting/:id" element={<NewMeetingPage />} />
         <Route path="/meeting/:id" element={<MeetingPage />} />
+        <Route path="/event/:id" element={<EventPage />} />
         {/* Redirect all unmatched routes to /login */}
         <Route path="*" element={<Navigate replace to="/login" />} />
       </Routes>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export { LoginPage } from "./login-page";
 export { NewMeetingPage } from "./new-meeting-page";
 export { HomePage } from "./home-page";
 export { MeetingPage } from "./meeting-page";
+export { EventPage } from "./event-page";
