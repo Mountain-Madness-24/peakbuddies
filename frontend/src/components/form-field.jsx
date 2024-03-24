8c2829f08aa41fd68a801df64eff76747a88984d
Kian Hosseinkhani <kian_hosseinkhani@sfu.ca>
2024-03-24 05:34:44 -0700

    added map to create event

--- frontend/src/components/form-field.jsx
+++ frontend/src/components/form-field.jsx
@@ -1,13 +1,13 @@
 import styles from "./form-field.module.scss";
 
-export const FormField = ({ type = "text", label, id, name }) => {
+export const FormField = ({ type = "text", label, id, name, onChange }) => {
   return (
     <fieldset className={styles.formField}>
       {label && <label htmlFor={id}>{label}</label>}
       {type === "textarea" ? (
         <textarea id={id} name={name}></textarea>
       ) : (
-        <input type={type} id={id} name={name} />
+        <input type={type} id={id} name={name} onChange={onChange} />
       )}
     </fieldset>
   );
